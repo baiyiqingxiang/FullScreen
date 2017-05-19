14b81f691b1bdcb3a2a3c7b3cbdeaeb6c9cfda00
baiyiqingxiang <huguopeng4102@163.com>
2017-05-19 20:52:00 +0800

    Initial Commit

---
+++ FullScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FullScreen
+//
+//  Created by 白衣卿相 on 2017/5/19.
+//  Copyright © 2017年 白衣卿相. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
